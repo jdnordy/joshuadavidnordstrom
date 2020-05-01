f570c9261aa7e974fa383f31cfa02f3c46d16180
Joshua Nordstrom <jdnordy@gmail.com>
2020-05-01 12:12:46 -0700

    setup next.js

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <a href="/about">about</a>
+    </div>
+  );
+};
+
+export default HomePage;
