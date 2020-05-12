0fc4396280e50c53d90b22163e7cb967c34c1c97
Joshua Nordstrom <jdnordy@gmail.com>
2020-05-12 12:14:06 -0700

    updated how the title works for each page

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -7,12 +7,16 @@ import Navbar from '../Navbar';
 
 type Props = {
   page: string;
+  subPage?: string;
 };
 
-const Layout: React.FC<Props> = ({ page, children }) => {
+const Layout: React.FC<Props> = ({ page, children, subPage }) => {
+  const title = `${page.split('')[0].toUpperCase() + page.slice(1)}${
+    subPage ? '/' + subPage : ''
+  }`;
   return (
     <>
-      <title>{page}</title>
+      <title>{title}</title>
       <div className={styles.main_container}>{children}</div>
       <Navbar page={page} />
     </>
