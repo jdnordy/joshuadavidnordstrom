c7ae5b5cb3e9f16b09430b03f511743f0acba37c
Joshua Nordstrom <jdnordy@gmail.com>
2020-05-12 12:28:44 -0700

    updated meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="A public archives of creations and writings by a problem solver and perpetual learner."
+          />
+          <meta property="og:image" content="/joshua-bw.jpg" />
+        </Head>
         <body>
           <Main />
           <NextScript />
