97179e733b2290b0826122e5f5e1d34ecc7f4a8c
Joshua Nordstrom <jdnordy@gmail.com>
2020-05-17 12:57:37 -0700

    improved analytics to track which page user is on

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 // COMPONENETS
 import { Navbar } from '../';
 // STYLES
@@ -14,10 +15,29 @@ const Layout: React.FC<Props> = ({ page, children, subPage }) => {
   const title = `${page.split('')[0].toUpperCase() + page.slice(1)}${
     subPage ? ' - ' + subPage : ''
   }`;
+
+  const router = useRouter();
+
+  const google = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-166852828-1', {
+      page_title: ${subPage ? subPage : page},
+      page_path: ${router.asPath}
+    });
+  `;
+
   return (
     <>
       <Head>
         <title>{title}</title>
+        <script
+          dangerouslySetInnerHTML={{ __html: google }}
+          key="google"
+        ></script>
       </Head>
       <div className={styles.main_container}>{children}</div>
       <Navbar page={page} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-const google = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){
-    dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-  gtag('config', 'UA-166852828-1');
-`;
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-166852828-1"
           ></script>
-          <script dangerouslySetInnerHTML={{ __html: google }}></script>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta
             name="description"
             content="A public archives of creations and writings by a problem solver and perpetual learner."
