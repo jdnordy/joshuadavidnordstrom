1d9e114eb088b4e26f0b3eb41f01389149bf6652
Joshua Nordstrom <jdnordy@gmail.com>
2020-05-26 14:23:42 -0700

    updated Layout to include OgTags

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import Head from 'next/head';
 // COMPONENETS
-import { Navbar, OgTags } from '../';
+import { Navbar, OgTags, GoogleAnalytics } from '../';
 // STYLES
 import styles from './styles.module.scss';
 
 type Props = {
   page: string;
   subPage?: string;
+  og: {
+    ogTitle: string;
+    ogDescription: string;
+    ogImage?: string;
+    article: boolean;
+  };
 };
 
-const Layout: React.FC<Props> = ({ page, children, subPage }) => {
+const Layout: React.FC<Props> = ({ page, children, subPage, og }) => {
   const title = `${page.split('')[0].toUpperCase() + page.slice(1)}${
     subPage ? ' - ' + subPage : ''
   }`;
 
   return (
     <>
+      <GoogleAnalytics title={title} />
+      <OgTags {...og} />
       <Head>
         <title>{title}</title>
       </Head>
