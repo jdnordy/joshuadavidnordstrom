aa31b284aa8a1fbefc9285f545c16cb5b1fc62a9
Joshua Nordstrom <jdnordy@gmail.com>
2020-05-26 14:53:18 -0700

    updated og tags

--- components/OgTags/index.tsx
+++ components/OgTags/index.tsx
@@ -18,7 +18,11 @@ const OgTags: React.FC<Props> = ({
   const router = useRouter();
   return (
     <Head>
-      <meta property="og:url" content={router.asPath} key="og:url" />
+      <meta
+        property="og:url"
+        content={'https://jdnordstrom.com' + router.pathname}
+        key="og:url"
+      />
       <meta property="og:title" content={ogTitle} key="og:title" />
       <meta
         property="og:description"
@@ -27,9 +31,11 @@ const OgTags: React.FC<Props> = ({
       />
       <meta
         property="og:image"
-        content={ogImage ? ogImage : 'https://jdnordstrom.com/logo_v6.png'}
+        content={ogImage ? ogImage : 'https://jdnordstrom.com/joshua_og.jpeg'}
         key="og:image"
       />
+      <meta property="og:image:width" content="1200" key="og:image:width" />
+      <meta property="og:image:height" content="630" key="og:image:height" />
       <meta
         property="og:type"
         content={article ? 'article' : 'website'}
